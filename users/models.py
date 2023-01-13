c82c9c71f51f8aba72f3d3cac5d94d063a29a5c6
Hakan Bilgi <hknblg34@gmail.com>
2023-01-13 20:12:43 +0300

    pictures and pillow add

--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-from datetime import date
-
+# from django.contrib.auth.models import AbstractUser
+# from django.conf import settings
+# from datetime import date
+from django.contrib.auth.models import User
 
 # Normal default user ı (ekstra fieldlar eklemek için) kullanmayıp kendimiz bir custom user oluşturmak istiyorsak o zaman  AbstractUser dan inherit ederek oluşturabiliriz(exdending user table).Aşağıdaki gibi...ya da yeni bir tablo oluşturup bu tabloyu mevcut user tablosuna onetoone relations ile bağlarsak yeni tablomuza birçok yeni field (özellik) eklemiş oluruz...
 
@@ -17,4 +17,14 @@ from datetime import date
 #   def __str__(self):
 #       return "{}".format(self.email)
 
-
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=30, blank=True, null=True)
+    avatar = models.ImageField(upload_to="pictures", default="avatar.png")
+    bio = models.TimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile'"
+    
+    
+ 
