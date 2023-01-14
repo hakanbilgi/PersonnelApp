6599a260294994ad9f0b18929e8f0765de06a98e
Hakan Bilgi <hknblg34@gmail.com>
2023-01-14 21:59:08 +0300

    ...

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
+
 from dj_rest_auth.serializers import TokenSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
